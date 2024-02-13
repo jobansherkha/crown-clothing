4f7faa73a61876565e6b47d831326ce0f87cad48
jobansherkha <jobansherkha@gmail.com>
2024-02-13 10:46:38 -0500

    updating our app with the categorygrid component and categoryitem component

---
+++ src/components/CategoryGrid/CategoryGrid.js
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import { CategoryItem } from '../CategoryItem/CategoryItem';
+export const CategoryGrid = ({categories}) => {
+  return (
+    <div className="categories-container">
+      {categories.map((category) => {
+        return (
+          <CategoryItem key={category.id} category={category}/>
+        );
+      })}
+    </div>
+  )
+}
